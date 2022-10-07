46f5222c93f804aeb84ad5e99bccf014137859fb
nodemailer <nodemailer143@gmail.com>
2022-10-07 20:28:49 +0530

    env

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   env: {
     MAIL: `nodemailer143@gmail.com`,
     PASS: `srrnmrwwiaamrecd`,
-    URL: "https://agumentik-project-suraj745.vercel.app/",
+    URL: "https://agumentik-main-project.vercel.app/",
     MY_MAIL: "surajarya745@gmail.com",
     RECAPTCHA: `6LcKbekhAAAAAPp29-83upFuPt37YaZVWayAnyKV
 
